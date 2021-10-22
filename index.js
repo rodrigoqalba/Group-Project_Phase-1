ae5b948e49939b133f11232953d0a8a034b9d84f
Joshua <jdhawks@gmail.com>
2021-10-22 16:23:50 -0500

    skeleton

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-    .then(resp => resp.json())
-    .then(data => console.log(data))
-})
+document.addEventListener("DOMContentLoaded", getData)
+const BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+
+let dataHolder = [];
+
+function getData(){
+    fetch(BASE_URL)
+    .then((resp) => resp.json())
+    .then(data => dataFeeder(data));
+}
+function dataFeeder(data){
+    data.forEach(drinkyDrinks);
+    makeDataGlobal(data);
+}
+function makeDataGlobal(data){
+    dataHolder = [...data]
+    return dataHolder;
+}
+
+function drinkyDrinks (data){
+    const drinksImg = document.createElement("img")
+    drinksImg.src = data.image
+    ramenMenu.append(drinksImg)
+}
