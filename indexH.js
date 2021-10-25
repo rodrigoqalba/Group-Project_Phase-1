35627452c1c7d7a139e8cfc90d08e772e8b2b0cf
Joshua <jdhawks@gmail.com>
2021-10-25 12:01:27 -0500

    Josh Changes

--- indexH.js
+++ indexH.js
@@ -1,22 +1,40 @@
 const randomBASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 const baseURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
-// let dataHolder = [];
-
 const drinkMenu = document.querySelector("#drink-menu")
+let dataHolder= [];
 
 document.addEventListener("DOMContentLoaded", () => {
     fetch(baseURL)
-    .then((resp) => resp.json())
-    .then(data => displayDrinks(data))
-},
-
-function displayDrinks(drinks) {
-    // const drinkMenu = document.getElementById("drink-menu")
-    drinks.forEach(drink => {
-        const menuImage = document.createElement("img")
-        menuImage.src = drink.strDrinkThumb
-        drinkMenu.append(menuImage)
-        console.log(menuImage)
-    })
+    .then(resp => resp.json())
+    .then(drinks => {
+        drinksArray = Object.entries(drinks.drinks)
+        console.log(drinksArray)
+        dataFeeder(drinksArray)
 })
+})
+
+function dataFeeder(drinksArray){
+    drinksArray.forEach(displayDrinks);
+    makeDataGlobal(drinksArray);
+}
+
+function makeDataGlobal(drinksArray){
+    dataHolder = [...drinksArray]
+    return dataHolder;
+}
+
+function displayDrinks(drinksArray) {
+        const menuImage = document.createElement("img")
+        menuImage.src = drinksArray[1].strDrinkThumb
+
+        menuImage.addEventListener('click', () => {
+            document.querySelector("#drink-img").src = drinksArray[1].strDrinkThumb;
+            document.querySelector("#drink-name").innerHTML = drinksArray[1].strDrink;
+            document.querySelector("#drink-glass").innerHTML = drinksArray[1].strGlass;
+            document.getElementById("ing").innerHTML = `${drinksArray[1].strIngredient1}, ${drinksArray[1].strIngredient2}, ${drinksArray[1].strIngredient3}`;
+            document.getElementById("drink-inst").innerHTML = drinksArray[1].strInstructions;
+        })
+        drinkMenu.append(menuImage)
+}
+
